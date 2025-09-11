17351bcd77bd4e3a6306da239d0a3c0f47532746
Yashpal-vaghela <vaghela9632@gmail.com>
2025-09-11 12:37:41 +0530

    all urls update

--- home/urls.py
+++ home/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name='home'
 urlpatterns = [
     path('',views.home,name="home"),
-    path('about-us',views.about,name="about"),
-    path('contact-us',views.contact,name="contact"),
-    path('support',views.support,name="support"),
-    path('material',views.material,name="material"),
-    path('products',views.product,name="product"),
-    path('product/<slug:slug>',views.singleproduct,name="singleproduct"),
-    path('blogs',views.blog,name="blog"),
+    path('about-us/',views.about,name="about"),
+    path('contact-us/',views.contact,name="contact"),
+    path('support/',views.support,name="support"),
+    path('material/',views.material,name="material"),
+    path('products/',views.product,name="product"),
+    path('product/<slug:slug>/',views.singleproduct,name="singleproduct"),
+    path('blogs/',views.blog,name="blog"),
     path('blogs/<slug:slug>/',views.blog_detail,name="blog_detail")
 ]
